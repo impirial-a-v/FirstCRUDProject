4a2dfa8901eca34009a0bbd83f14b9a98906655b
Impirial <xedalx2@gmail.com>
2023-05-24 23:29:43 +0300

    My Project

---
+++ src/main/java/gg/alex/spring/dao/PersonDAO.java
@@ -0,0 +1,148 @@
+package gg.alex.spring.dao;
+
+import gg.alex.spring.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private int PEOPLE_COUNT;
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+           e.printStackTrace();
+        }
+    }
+
+
+    public List<Person> index(){
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+           ResultSet resultSet = statement.executeQuery(SQL);
+
+           while (resultSet.next()){
+               Person person = new Person();
+
+               person.setId(resultSet.getInt("id"));
+               person.setAge(resultSet.getInt("age"));
+               person.setName(resultSet.getString("pname"));
+               person.setEmail(resultSet.getString("email"));
+
+               people.add(person);
+           }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return people;
+    }
+
+       public Person show(int id){
+
+        Person person = null;
+           try {
+               PreparedStatement preparedStatement =
+                       connection.prepareStatement("SELECT * FROM Person WHERE id=?");
+               preparedStatement.setInt(1, id);
+
+               ResultSet resultSet = preparedStatement.executeQuery();
+               resultSet.next();
+
+               person = new Person();
+
+               person.setId(resultSet.getInt("id"));
+               person.setAge(resultSet.getInt("age"));
+               person.setName(resultSet.getString("pname"));
+               person.setEmail(resultSet.getString("email"));
+
+
+
+           } catch (SQLException e) {
+               throw new RuntimeException(e);
+           }
+
+           return person;
+       }
+
+    public void save(Person person){
+
+        try {
+
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Person VALUES (1, ?, ?, ?)");
+
+                preparedStatement.setString(1, person.getName());
+                preparedStatement.setString(3, person.getEmail());
+                preparedStatement.setInt(2, person.getAge());
+
+                preparedStatement.executeUpdate();
+/*
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'"+ person.getName()+"',"+person.getAge()+",'"+person.getEmail()+"')" ;
+            statement.executeUpdate(SQL);
+
+*/
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public void update(int id, Person updatePerson) {
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET  pname=?, age=?, email=? WHERE id=?");
+
+            preparedStatement.setString(1, updatePerson.getName());
+            preparedStatement.setString(3, updatePerson.getEmail());
+            preparedStatement.setInt(2, updatePerson.getAge());
+            preparedStatement.setInt(4, id);
+
+        preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public void delete(int id) {
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("DELETE FROM Person WHERE id=?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
---
+++ src/main/java/gg/alex/spring/models/Person.java
@@ -0,0 +1,59 @@
+package gg.alex.spring.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "Name not should be empty")
+    @Size(min=2, max = 30, message="name should be between 2 and 30 characters")
+    private String name;
+@Min(value = 0, message = "age should be greater than 0")
+    private int age;
+@NotEmpty(message = "e-mail should not to be empty")
+@Email(message = "E-mail should be valid")
+    private String email;
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Person(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
